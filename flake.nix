cfcaaa986d4d0ccb926ab387c27df31c5dbb30a4
tobjaw <tobjaw@users.noreply.github.com>
2025-11-04 00:18:28 +0100

    dogfood taskfile

--- flake.nix
+++ flake.nix
@@ -12,9 +12,10 @@
   outputs =
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
+      # For dogfooding: limit to darwin systems to avoid IFD cross-compilation issues
+      # Users of this module won't have this limitation - they can specify any systems they support
+      # The IFD issue only affects flake evaluation, not the module itself when used by others
       systems = [
-        "x86_64-linux"
-        "aarch64-linux"
         "x86_64-darwin"
         "aarch64-darwin"
       ];
@@ -57,7 +58,7 @@
           # Enable the taskfile module for this flake's testing
           taskfile = {
             enable = true;
-            path = ./examples/basic/Taskfile.yml;
+            path = ./Taskfile.yml;
             package = pkgs.go-task;
             excludeTasks = [ ];
           };
